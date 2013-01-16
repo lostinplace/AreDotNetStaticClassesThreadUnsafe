730a1281b46e54a2dc2ec53534c8f93b96881bd0
Chris Wheeler <cmwhee@gmail.com>
2013-01-15 22:58:06 -0500

    Initial Commit

---
+++ SimpleTests/UnitTest1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleTests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+      
+      try
+      {
+        paralleltest(delegate(){
+          Random rnd = new Random();
+          ThisIsNotTheMostUnsafeLibraryInTheWorld.UnsafeClass.First_Offender(Convert.ToInt32((rnd.NextDouble() * 10)));
+        });
+      }
+      catch (Exception ex)
+      {
+        Assert.Fail(ex.Message);
+        throw ex;
+      }
+    }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+      
+      try
+      {
+        paralleltest(delegate()
+        {
+          Random rnd = new Random();
+          int x= Convert.ToInt32((rnd.NextDouble() * 10));
+          ThisIsNotTheMostUnsafeLibraryInTheWorld.UnsafeClass.Second_Offender(ref x);
+        });
+      }
+      catch (Exception ex)
+      {
+        Assert.Fail(ex.Message);
+        throw ex;
+      }
+    }
+
+    [TestMethod]
+    public void TestMethod3()
+    {
+      try
+      {
+        paralleltest(delegate()
+        {
+          try
+          {
+            ThisIsNotTheMostUnsafeLibraryInTheWorld.UnsafeClass.Third_Offender();
+          }
+          catch (Exception ex)
+          {
+            Assert.Fail();
+            throw ex;
+          }
+          
+        });
+      }
+      catch (Exception ex)
+      {
+        Assert.Fail(ex.Message);
+        throw ex;
+      }
+    }
+    
+    public static Task[] buildparalleltest(Action anAction)
+    {
+      List<Task> threadList = new List<Task>();
+      
+      
+      for (int i = 0; i < 1000; i++)
+      {
+        threadList.Add(new Task(delegate()
+        {
+          anAction();
+        }));
+      }
+      return threadList.ToArray();
+    }
+
+  }
+}
---
+++ ThisIsNotTheMostUnsafeLibraryInTheWorld/offend.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThisIsNotTheMostUnsafeLibraryInTheWorld
+{
+  public static class UnsafeClass
+    {
+      public static void First_Offender(int x)
+      {
+        int y;
+        try
+        {
+          y = x;
+          x++;
+          x++;
+          System.Threading.Thread.Sleep(10);
+          x++;
+          x++;
+
+          if (x != y + 4) throw new ThreadStateException("ZOMG THe sky is purple.... something has gone terribly wrong");
+        }
+        catch (Exception ex)
+        {
+          throw ex;
+        }
+      }
+      public static void Second_Offender(ref int x)
+      {
+        int y;
+        try
+        {
+          y = x;
+          x++;
+          x++;
+          System.Threading.Thread.Sleep(10);
+          x++;
+          x++;
+
+          if (x != y + 4) throw new ThreadStateException("ZOMG THe sky is purple.... something has gone terribly wrong");
+        }
+        catch (Exception ex)
+        {
+          throw ex;
+        }
+      }
+
+      static int tracker = 0;
+      
+
+      public static void Third_Offender()
+      {
+        int y;
+        try
+        {
+          y = tracker;
+          tracker++;
+          tracker++;
+          System.Threading.Thread.Sleep(10);
+          tracker++;
+          tracker++;
+          if (y > 100) throw new ThreadInterruptedException("I'm done mothafucka " + y.ToString());
+        }
+        catch (Exception ex)
+        {
+          throw ex;
+        }
+      }
+
+      //wait for it
+      static double modifier = 0.0001;
+
+      /// <summary>
+      /// In which my laptop becomes a person and learns language, so that it can say, "Fuck You chris, fuck you"
+      /// </summary>
+      /// <param name="x"></param>
+      public static void Fourth_Offender(ref int x)
+      {
+        try
+        {
+          Random rnd = new Random();
+          int y = x;
+          double chance = rnd.NextDouble();
+          if (chance - modifier > 0.25)
+          {
+            modifier += 0.0001;
+            Thread newThread = new Thread(delegate()
+            {
+              Fourth_Offender(ref y);
+            });
+          }
+
+        }
+        catch (Exception ex)
+        {
+          throw ex;
+        }
+      }
+    }
+}
